6361045f8ee5ac4ab742d8c56f9a3a11a0accabe
Tommy Gaessler <tommy.gaessler@gmail.com>
2016-11-30 10:58:29 -0700

    finished setup and connected with serverside

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import axios from 'axios';
 import { Component } from 'react';
 
 class Auth extends Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+
+  componentDidMount() {
+    var code = this.props.location.query.code;
+
+    axios.post('http://localhost:3000/auth', {
+      code
+    })
+    .then((response) => {
+      console.log(response.data);
+      this.setState({
+        "authResponse":response.data.username
+      })
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
   render() {
     return (
-      <h1>You are authenticated</h1>
+      <div>
+        <h1>You are authenticated</h1>
+        <span>{this.state.authResponse}</span>
+      </div>
     )
   }
 }
--- src/components/home.js
+++ src/components/home.js
@@ -6,7 +6,7 @@ class Home extends Component {
     return (
       <div>
         <h1>Fundsplash</h1>
-        <a href="https://unsplash.com/oauth/authorize?client_id=3500c188f8334662d427d07186a12a9ed4b32e0d30c08c85487bc8a20120f107&redirect_uri=http://localhost:8080/auth&response_type=code&scope=public+write_likes+write_followers+read_user">Login with Unsplash</a>
+        <a href="https://unsplash.com/oauth/authorize?client_id=2c9aa4160523a0848dc028d33ed50635abaa5d8a5f360a58d5760d8ebdedfac6&redirect_uri=http://localhost:8080/auth&response_type=code&scope=public+write_likes+write_followers+read_user">Login with Unsplash</a>
       </div>
     )
   }
