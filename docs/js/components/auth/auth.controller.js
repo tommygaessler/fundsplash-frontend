3db0b2776432d8aa4e1da4b1b858593258c6bd7a
Tommy Gaessler <tommy.gaessler@gmail.com>
2016-12-01 17:22:18 -0700

    finally got oauth truly working

--- docs/js/components/auth/auth.controller.js
+++ docs/js/components/auth/auth.controller.js
@@ -6,14 +6,21 @@
     .module('fundsplash.components.auth', [])
     .controller('authController', authController);
 
-  authController.$inject = ['$scope', '$http'];
+  authController.$inject = ['$scope', '$http', '$rootScope'];
 
-  function authController($scope, $http) {
+  function authController($scope, $http, $rootScope) {
     /*jshint validthis: true */
 
     $http.get('http://localhost:3000/profile')
-    .then((profile) => {
-      console.log(profile);
+    .then((user) => {
+      console.log(user.data);
+
+      const token = user.data.access_token;
+      sessionStorage.setItem('token', token);
+
+      console.log(sessionStorage.getItem('token'));
+
+      $rootScope.user = user.data;
     })
     .catch((error) => {
       console.log(error);
--- docs/js/components/create/create.controller.js
+++ docs/js/components/create/create.controller.js
@@ -6,10 +6,16 @@
     .module('fundsplash.components.create', [])
     .controller('createController', createController);
 
-  createController.$inject = ['$scope', '$http'];
+  createController.$inject = ['$scope', '$http', '$auth'];
 
-  function createController($scope, $http) {
+  function createController($scope, $http, $auth) {
     /*jshint validthis: true */
+
+    $scope.authenticate = function() {
+      $auth.authenticate('unsplash').then(result => {
+        console.log(result)
+      })
+    }
   }
 
 })();
