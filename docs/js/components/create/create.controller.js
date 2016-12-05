3dcb38a192c7d5ac7ad8ba9d5384a6d9dd8561f6
Tommy Gaessler <tommy.gaessler@gmail.com>
2016-12-05 14:37:16 -0700

    added stripe integration

--- docs/js/components/create/create.controller.js
+++ docs/js/components/create/create.controller.js
@@ -11,13 +11,35 @@
   function createController($scope, $http, $auth, $rootScope) {
     /*jshint validthis: true */
 
+    this.sample_photos = [];
+
     $scope.authenticate = function() {
       $auth.authenticate('unsplash')
       .then(user => {
         $rootScope.user = user.data.data;
         $rootScope.photos = user.data.photos;
+        sessionStorage.setItem('photographer_id', user.data.data.id);
       });
     };
+
+    this.toggle = false;
+
+    this.photos = function(photo) {
+
+      if (this.sample_photos.includes(photo)) {
+        // toggle
+        this.sample_photos.splice(this.sample_photos.indexOf(photo), 1);
+        photo.toggle = !photo.toggle;
+      } else if (this.sample_photos.length === 3) {
+        // dont toggle
+        alert('only 3 photos');
+      } else {
+        // toggle
+        this.sample_photos.push(photo);
+        photo.toggle = !photo.toggle;
+      }
+      console.log(this.sample_photos);
+    }
 
     this.campaign = function() {
 
@@ -25,20 +47,27 @@
         location: this.location,
         goal: this.goal,
         description: this.description,
-        photographer_id: $rootScope.user.id
+        photographer_id: $rootScope.user.id,
+        sample_photo_1: this.sample_photos[0].urls.regular,
+        sample_photo_2: this.sample_photos[1].urls.regular,
+        sample_photo_3: this.sample_photos[2].urls.regular
       }
 
-      console.log(data);
-
       $http.post('http://localhost:3000/campaign', data)
-      .then((data) => {
-        console.log(data);
+      .then((created) => {
+        console.log(created);
+        $rootScope.created = true;
+
+        const photographer_id = sessionStorage.getItem('photographer_id');
+
+        // make api call to database to grab values
+        $http.get(`http://localhost:3000/campaign/${photographer_id}`)
+        .then((campaign) => {
+          console.log(campaign);
+          $rootScope.campaign = campaign.data;
+        });
       });
     }
   }
-
-  window.addEventListener('popstate', function() {
-    handler.close();
-  });
 
 })();
